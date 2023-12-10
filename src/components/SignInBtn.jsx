fc45bf69560608003ac63a6e496aed8a964a9cb2
Anh Khoa <mr.khoacoding@gmail.com>
2023-12-11 04:08:32 +0700

    Day50

--- src/components/SignInBtn.jsx
+++ src/components/SignInBtn.jsx
@@ -3,6 +3,7 @@ import { signIn } from "next-auth/react";
 import React from "react";
 
 export function SignInGoogleBtn() {
+  console.log(process.env.NEXTAUTH_URL);
   return (
     <button
       onClick={() => signIn("google")}
