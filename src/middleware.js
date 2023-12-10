7b765e18db6e0ec6df43addd34f7db974514d3d0
Anh Khoa <mr.khoacoding@gmail.com>
2023-12-11 05:23:18 +0700

    Day50

--- src/middleware.js
+++ src/middleware.js
@@ -33,18 +33,17 @@ export default async function middleware(request) {
     cookieName: process.env.NEXT_PUBLIC_SESSION,
     secret: process.env.NEXTAUTH_SECRET,
   });
-  // console.log(process.env.NEXTAUTH_URL);
 
-  // if (pathname.startsWith(`/${lang}/auth`)) {
-  //   if (jwt) {
-  //     return NextResponse.redirect(new URL(`/${lang}/profile`, request.url));
-  //   } else {
-  //     const lang = pathname.startsWith("/vi/auth") ? "vi" : "en";
-  //     const response = NextResponse.next();
-  //     response.cookies.set("lang", lang || i18n.defaultLocale);
-  //     return response;
-  //   }
-  // }
+  if (pathname.startsWith(`/${lang}/auth`)) {
+    if (jwt) {
+      return NextResponse.redirect(new URL(`/${lang}/profile`, request.url));
+    } else {
+      const lang = pathname.startsWith("/vi/auth") ? "vi" : "en";
+      const response = NextResponse.next();
+      response.cookies.set("lang", lang || i18n.defaultLocale);
+      return response;
+    }
+  }
   // if (pathname.startsWith(`/${lang}/profile`)) {
   //   if (!jwt) {
   //     const response = NextResponse.redirect(
