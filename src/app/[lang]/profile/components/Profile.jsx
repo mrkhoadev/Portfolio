de0bedafd3ebffc153dc4598a6adbf4916048aad
Anh Khoa <mr.khoacoding@gmail.com>
2023-12-11 03:41:51 +0700

    Day50

--- src/app/[lang]/profile/components/Profile.jsx
+++ src/app/[lang]/profile/components/Profile.jsx
@@ -7,8 +7,8 @@ import LogoutBtn from "./LogoutBtn";
 export default function Profile({ dataLang, session }) {
   return (
     <div className="flex flex-col gap-y-4">
-      <h2>{session.name}</h2>
-      <h2>{session.email}</h2>
+      <h2>{session?.name}</h2>
+      <h2>{session?.email}</h2>
       <>
         {session?.provider === "github" ? (
           <div className="min-h-[50px]">
