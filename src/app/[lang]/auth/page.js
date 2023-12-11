4a0f73e175aff9985c9e6065a9f2cb6bc50cf6bc
Anh Khoa <mr.khoacoding@gmail.com>
2023-12-11 18:47:24 +0700

    fix bug

--- src/app/[lang]/auth/page.js
+++ src/app/[lang]/auth/page.js
@@ -1,9 +1,10 @@
+import { SignInGithubBtn, SignInGoogleBtn } from "@/components/SignInBtn";
 import React from "react";
-import AuthForm from "./components/AuthForm";
-import { getDictionary } from "@/Utils/dictionary";
+// import AuthForm from "./components/AuthForm";
+// import { getDictionary } from "@/Utils/dictionary";
 // import { SignInGithubBtn, SignInGoogleBtn } from "@/components/SignInBtn";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./[...nextauth]/route";
 
 export const metadata = {
   title: "Đăng nhập / Đăng ký",
@@ -11,13 +12,19 @@ export const metadata = {
 };
 
 export default async function Auth({ params: { lang } }) {
-  const dataLang = await getDictionary(lang);
-  const session = await getServerSession(authOptions);
+  // const dataLang = await getDictionary(lang);
+  // const session = await getServerSession(authOptions);
   return (
     <>
       <main>
         <div className="mx-auto flex justify-center items-center min-h-[650px] flex-col gap-4">
-          <AuthForm lang={lang} dataLang={dataLang} session={session} />
+          {/* <AuthForm lang={lang} dataLang={dataLang} session={session} /> */}
+          <div className="min-h-[50px]">
+            <SignInGoogleBtn />
+          </div>
+          <div className="min-h-[50px]">
+            <SignInGithubBtn />
+          </div>
         </div>
       </main>
     </>
